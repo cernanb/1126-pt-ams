486a1264ad969816a4243ef8b59cc374d29931f0
cernanb <cernanb@gmail.com>
2019-07-16 14:47:25 -0700

    Build students model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_16_185312) do
+ActiveRecord::Schema.define(version: 2019_07_16_214251) do
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "teacher_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "teachers", force: :cascade do |t|
     t.string "name"
